4f448fe764821b26b45033bfd3498446767bb228
electr0Nik <Zkorpio@freenet.de>
2015-11-24 22:01:17 +0100

    * deleting _old_-tutorial files * deleting obsolete startForm.html * pom.xml - adding profile * fixing wrong-class for LoggerFactory * modifying logback-configuration * modifying bpmn-definition

--- src/main/java/com/github/electr0nik/camunda/purchaseorder/delegate/PurchaseOrderJavaDelegate.java
+++ src/main/java/com/github/electr0nik/camunda/purchaseorder/delegate/PurchaseOrderJavaDelegate.java
@@ -1,17 +1,23 @@
 package com.github.electr0nik.camunda.purchaseorder.delegate;
 
-import com.github.electr0nik.camunda.purchaseorder.form.MealForm;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.electr0nik.camunda.purchaseorder.form.MealForm;
 
 /**
  * Created by nik on 24.11.15.
  */
 public class PurchaseOrderJavaDelegate implements JavaDelegate {
 
+  private final static Logger LOGGER = LoggerFactory.getLogger(PurchaseOrderJavaDelegate.class);
 
   @Override
   public void execute(DelegateExecution execution) throws Exception {
     MealForm mealForm = (MealForm) execution.getVariable("mealForm");
+
+    execution.getVariables().forEach((key, value) -> LOGGER.info(String.format("key: %s, value: %s", key, value)));
   }
 }
--- src/main/java/com/github/electr0nik/camunda/tutorial/PurchaseOrderApplication.java
+++
@@ -1,9 +0,0 @@
-package com.github.electr0nik.camunda.tutorial;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Purchase Order App")
-public class PurchaseOrderApplication extends ServletProcessApplication {
-	// empty implementation
-}
