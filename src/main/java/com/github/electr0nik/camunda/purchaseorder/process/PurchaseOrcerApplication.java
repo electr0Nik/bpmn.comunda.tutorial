11b940a93a68adf2be34dcce4d83d8b6fa8509b6
electr0Nik <Zkorpio@freenet.de>
2015-11-25 01:44:38 +0100

    * adding missing process starter

---
+++ src/main/java/com/github/electr0nik/camunda/purchaseorder/process/PurchaseOrcerApplication.java
@@ -0,0 +1,9 @@
+package com.github.electr0nik.camunda.purchaseorder.process;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class PurchaseOrcerApplication extends ServletProcessApplication {
+
+}
