93cda8951c3740f24cda8ec87acc30d77deaf901
electr0nik <Zkorpio@freenet.de>
2015-11-25 02:04:32 +0100

    * change jul to logback

--- src/main/java/com/github/electr0nik/camunda/purchaseorder/delegate/MealObjectEnricher.java
+++ src/main/java/com/github/electr0nik/camunda/purchaseorder/delegate/MealObjectEnricher.java
@@ -3,19 +3,19 @@ package com.github.electr0nik.camunda.purchaseorder.delegate;
 import com.github.electr0nik.camunda.purchaseorder.model.Meal;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by nik on 24.11.15.
  */
 public class MealObjectEnricher implements JavaDelegate {
 
-  private final static Logger LOGGER = Logger.getLogger(MealObjectEnricher.class.getName());
+  private final static Logger LOGGER = LoggerFactory.getLogger(MealObjectEnricher.class);
   private final static Properties properties = new Properties();
 
   private final static String DEFAULT_PROPERTY_NAME_PREFIX = "default.price.";
@@ -36,7 +36,7 @@ public class MealObjectEnricher implements JavaDelegate {
       try {
         tmpAmount = Long.parseLong(it.getAmount());
       } catch (NumberFormatException nfe) {
-        LOGGER.log(Level.SEVERE, "an exception was thrown", nfe);
+        LOGGER.error(nfe.getMessage(), nfe);
         tmpAmount = 1L;
       }
       it.setPriceInCent(tmpAmount * tmpValue + (((tmpAmount * tmpValue) * DEFAULT_EXTRA_CHARGE) / 100));
@@ -45,11 +45,16 @@ public class MealObjectEnricher implements JavaDelegate {
     LOGGER.info("end enrichment!");
   }
 
-  private void populatedProperties(final String propertySource) throws IOException {
+  private void populatedProperties(final String propertySource) {
+    LOGGER.info("Begin populatedProperties!");
     // get resource from classpath.. we need this in web-environment
-    final InputStream input = this.getClass().getClassLoader().getResourceAsStream(DEFAULT_PROPERTY_SOURCE);
+    final InputStream input = this.getClass().getClassLoader().getResourceAsStream(propertySource);
     // load a properties file
-    properties.load(input);
+    try {
+      properties.load(input);
+    } catch (IOException e) {
+      LOGGER.error(e.getMessage(), e);
+    }
     properties.stringPropertyNames().forEach(propName -> {
       LOGGER.info(String.format("set properties: \nkey: %s \tvalue: %s ", propName, properties.getProperty(propName)));
     });
--- src/main/java/com/github/electr0nik/camunda/purchaseorder/delegate/PurchaseOrderJavaDelegate.java
+++ src/main/java/com/github/electr0nik/camunda/purchaseorder/delegate/PurchaseOrderJavaDelegate.java
@@ -3,15 +3,16 @@ package com.github.electr0nik.camunda.purchaseorder.delegate;
 import com.github.electr0nik.camunda.purchaseorder.model.Meal;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.logging.Logger;
 
 /**
  * Created by nik on 24.11.15.
  */
 public class PurchaseOrderJavaDelegate implements JavaDelegate {
 
-  private final static Logger LOGGER = Logger.getLogger(PurchaseOrderJavaDelegate.class.getName());
+  private final static Logger LOGGER = LoggerFactory.getLogger(PurchaseOrderJavaDelegate.class);
 
   @Override
   public void execute(DelegateExecution execution) throws Exception {
