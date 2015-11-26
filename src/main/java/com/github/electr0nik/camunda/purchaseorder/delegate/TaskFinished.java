727e5efd9e4527389c63a2418904f92feb4adb81
electr0Nik <Zkorpio@freenet.de>
2015-11-26 15:50:08 +0100

    extend process
    delete obsolete html
    rename specific files

---
+++ src/main/java/com/github/electr0nik/camunda/purchaseorder/delegate/TaskFinished.java
@@ -0,0 +1,22 @@
+package com.github.electr0nik.camunda.purchaseorder.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TaskFinished implements JavaDelegate {
+
+  private final Logger LOGGER = LoggerFactory.getLogger(TaskFinished.class);
+
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    LOGGER.info("Begin TaskFinished!");
+
+    LOGGER.info("Task Finished!\n vars used: ");
+    execution.getVariables().forEach((key, value) -> LOGGER.info("key: " + key + "\t value: " + value));
+
+    LOGGER.info("End TaskFinished!");
+  }
+
+}
